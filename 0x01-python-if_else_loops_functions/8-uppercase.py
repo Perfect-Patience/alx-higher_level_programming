428211f2765745e3632432e2983e49e8e91340c5
Perfect Dah <patiencedah@gmail.com>
2022-06-29 13:05:53 -0700

    prints a string in uppercase followed by a new line

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/bin/usr/python3
+def uppercase(str):
+    for c in str:
+        c = ord(c)
+        if c >= 97 and c <= 122:
+            c -= 32
+        print("{}".format(chr(c)), end="")
+    print("{}".format('\n'), end="")
