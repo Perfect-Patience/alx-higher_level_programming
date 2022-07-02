629c3066003f4e4a8fa8178a0cc00d9c18e13d08
Perfect Dah <patiencedah@gmail.com>
2022-07-02 16:32:16 +0000

    corrects code

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def no_c(my_string):
+    j = 0
     for i in range(len(my_string)):
         if my_string[i] == 'c' or my_string[i] == 'C':
-            del(my_string[i])
+            continue
+        new_string[j] = my_string[i]
+        j++
     return my_string
