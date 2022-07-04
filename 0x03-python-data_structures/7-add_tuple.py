fd0c7c232ffa73cb5b65c64d6a983a9ab0a03c02
Perfect Dah <patiencedah@gmail.com>
2022-07-04 13:18:01 -0700

    adds 2 tuples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,14 +1,19 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    sum1 = 0
-    sum2 = 0
-    if (tuple_a[0] and tupl_b[0]:
-        sum1 = tuple_a[0] + tuple_b[0]
-    if tuple_a[1] == None:
-        tuple_a[1] = 0
-    if tuple_b[1] == None:
-        tuple_b[1] == 0
-    sum2 = tuple_a[1] + tuple_b[1]
-    print("({}, {})".format(sum1, sum2))
-
-
+    if len(tuple_a) == 0:
+        val1 = 0
+    else:
+        val1 = tuple_a[0]
+    if len(tuple_b) == 0:
+        val2 = 0
+    else:
+        val2 = tuple_b[0]
+    if len(tuple_a) != 2:
+        val3 = 0
+    else:
+        val3 = tuple_a[1]
+    if len(tuple_b) != 2:
+        val4 = 0
+    else:
+        val4 = tuple_b[1]
+    return (val1 + val2, val3 + val4)
