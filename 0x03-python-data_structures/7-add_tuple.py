996406190988a21f793999227081788081d422d5
Perfect Dah <patiencedah@gmail.com>
2022-07-05 07:42:03 +0000

    corrects if statements

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -8,11 +8,11 @@ def add_tuple(tuple_a=(), tuple_b=()):
         val2 = 0
     else:
         val2 = tuple_b[0]
-    if len(tuple_a) != 2:
+    if len(tuple_a) < 2:
         val3 = 0
     else:
         val3 = tuple_a[1]
-    if len(tuple_b) != 2:
+    if len(tuple_b) < 2:
         val4 = 0
     else:
         val4 = tuple_b[1]
