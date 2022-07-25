cf971d57de7daf3d369f5007d24af2cd98343089
Perfect Dah <patiencedah@gmail.com>
2022-07-25 02:33:23 +0000

    corrects indentation

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -3,5 +3,5 @@ def multiply_by_2(a_dictionary):
     list1 = list(a_dictionary)
     new_dict = dict([])
     for i in list1:
-       new_dict[i] = a_dictionary[i] * 2
+        new_dict[i] = a_dictionary[i] * 2
     return new_dict
