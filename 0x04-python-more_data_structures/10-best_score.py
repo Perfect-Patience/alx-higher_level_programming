6062175926dd3e321daaadbd103c2140942ac824
Perfect Dah <patiencedah@gmail.com>
2022-07-25 08:53:16 +0000

    includes the posiblilty of having an empty dicitonary

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+from asyncio.windows_events import NULL
+from contextlib import nullcontext
+from queue import Empty
+from requests import NullHandler
+
+
 def best_score(a_dictionary):
-    if a_dictionary is None:
+    if a_dictionary is None or  not a_dictionary:
         return None
     list1 = list(a_dictionary)
     max = list1[0]
